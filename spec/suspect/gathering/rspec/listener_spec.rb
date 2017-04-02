96005cf0cc92c7e856d8a540f24d1c5c47700b42
Pavlo Mitin <mitin.pavel@gmail.com>
2017-04-02 08:42:28 +0100

    Refactor specs: extract "append_to" custom matcher to StorageAppenderHelpers

--- spec/suspect/gathering/rspec/listener_spec.rb
+++ spec/suspect/gathering/rspec/listener_spec.rb
@@ -3,46 +3,45 @@ require 'suspect/gathering/rspec/listener'
 require 'suspect/storage/appender'
 require 'suspect/file_tree/git/snapshot'
 
+module StorageAppenderHelpers
+  extend ::RSpec::Matchers::DSL
+
+  matcher :append_to  do |appender|
+    match do |actual|
+      expect(appender).to receive(:append).with(instance_of(::Suspect::Gathering::RunInfo)) do |run_info|
+        @expected_run_info_data.each do |attribute, value|
+          expect(run_info.public_send(attribute)).to eq(value)
+        end
+      end
+
+      actual.call
+
+      true
+    end
+
+    chain(:with) do |options|
+      @expected_run_info_data = options
+    end
+
+    def supports_block_expectations?
+      true
+    end
+  end
+end
+
 RSpec.describe Suspect::Gathering::RSpec::Listener do
   let(:file_tree) { instance_double(::Suspect::FileTree::Git::Snapshot, modified_files: []) }
   let(:storage_appender) { instance_double(::Suspect::Storage::Appender) }
 
-  describe '#notification_names' do
-    it 'is only interested in "stop" notification' do
-      listener = described_class.new(file_tree, storage_appender)
-      expect(listener.notification_names).to eq(%i(stop))
-    end
-  end
-
   describe '#stop' do
-    RSpec::Matchers.define :append_to_storage do |appender|
-      match do |actual|
-        expect(appender).to receive(:append).with(instance_of(::Suspect::Gathering::RunInfo)) do |run_info|
-          @expected_run_info_data.each do |attribute, value|
-            expect(run_info.public_send(attribute)).to eq(value)
-          end
-        end
-
-        actual.call
-
-        true
-      end
-
-      chain(:with) do |options|
-        @expected_run_info_data = options
-      end
-
-      def supports_block_expectations?
-        true
-      end
-    end
+    include StorageAppenderHelpers
 
     let(:listener) { described_class.new(file_tree, storage_appender) }
 
     it 'stores file paths of failed examples' do
       expect do
         listener.stop examples_notification(failed_files: %w(/path/to/a_spec.rb /path/to/b_spec.rb))
-      end.to append_to_storage(storage_appender).with failed_files: %w(/path/to/a_spec.rb /path/to/b_spec.rb)
+      end.to append_to(storage_appender).with failed_files: %w(/path/to/a_spec.rb /path/to/b_spec.rb)
     end
 
     it 'stores file paths of modified files' do
@@ -50,7 +49,7 @@ RSpec.describe Suspect::Gathering::RSpec::Listener do
 
       expect do
         listener.stop examples_notification
-      end.to append_to_storage(storage_appender).with modified_files: %w(/path/to/a.rb /path/to/b.rb)
+      end.to append_to(storage_appender).with modified_files: %w(/path/to/a.rb /path/to/b.rb)
     end
 
     context '[no failed examples]' do
@@ -69,6 +68,13 @@ RSpec.describe Suspect::Gathering::RSpec::Listener do
     def example(file_path)
       instance_double(::RSpec::Core::Example,
                       file_path: file_path)
+    end
+  end
+
+  describe '#notification_names' do
+    it 'is only interested in "stop" notification' do
+      listener = described_class.new(file_tree, storage_appender)
+      expect(listener.notification_names).to eq(%i(stop))
     end
   end
 end
