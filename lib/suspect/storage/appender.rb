5fa0f239e79f4a65c201ddf67d7e0f5f7a83a7f2
Pavlo Mitin <mitin.pavel@gmail.com>
2017-04-11 16:13:47 +0100

    Compose a storage filename using actor_id, a storage version and *.ss extension

--- lib/suspect/storage/appender.rb
+++ lib/suspect/storage/appender.rb
@@ -6,21 +6,24 @@ require_relative 'dir_path'
 module Suspect
   module Storage
     class Appender
+      VERSION = '1'
+
       def initialize(opts)
         @writer = opts[:writer] || FlockWriter.new
         @path = opts[:path] || fail(ArgumentError, 'No path found')
+        @actor_id = opts[:actor_id] || fail(ArgumentError, 'No actor_id found')
       end
 
       def append(run_info)
-        filename = "#{create_full_path}/#{run_info.commit_hash}"
+        filename = "#{create_full_path}/#{actor_id}-#{VERSION}.ss"
         content = run_info.to_s
 
-        @writer.write filename, content
+        writer.write filename, content
       end
 
       private
 
-      attr_reader :writer, :path
+      attr_reader :writer, :path, :actor_id
 
       #TODO Extract dir creation.
       def create_full_path
--- spec/suspect/storage/appender_spec.rb
+++ spec/suspect/storage/appender_spec.rb
@@ -5,20 +5,21 @@ RSpec.describe Suspect::Storage::Appender do
   describe '#append' do
     before { allow(FileUtils).to receive :mkdir_p }
 
-    let(:run_info) { instance_double(::Suspect::Gathering::RunInfo, to_s: 'serialized run info', commit_hash: 'hash') }
+    let(:run_info) { instance_double(::Suspect::Gathering::RunInfo, to_s: 'serialized run info') }
     let(:writer) { instance_double(::Suspect::Storage::FlockWriter) }
 
     it 'appends serialized run info' do
       expect(writer).to receive(:write).with anything, 'serialized run info'
 
-      appender = described_class.new(path: '.', writer: writer)
+      appender = described_class.new(path: '.', writer: writer, actor_id: 'some id')
       appender.append run_info
     end
 
-    it 'compose a  storage file path concatenating the passed path and the current commit hash' do
-      expect(writer).to receive(:write).with '/a/path/to/store/hash', anything
+    it 'compose a storage file path concatenating the path, the storage version and the actor id' do
+      expect(described_class::VERSION).to eq('1')
+      expect(writer).to receive(:write).with '/a/path/to/store/12345-1.ss', anything
 
-      appender = described_class.new(path: '/a/path/to/store', writer: writer)
+      appender = described_class.new(path: '/a/path/to/store', writer: writer, actor_id: '12345')
       appender.append run_info
     end
   end
