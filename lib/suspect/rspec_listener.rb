bdeacb1ab4204bf0734e66a7e8256f4e9763a16d
Pavlo Mitin <mitin.pavel@gmail.com>
2017-05-19 06:49:18 +0100

    Refactor Suspect::RSpecListener extracting private methods

--- lib/suspect/rspec_listener.rb
+++ lib/suspect/rspec_listener.rb
@@ -29,22 +29,33 @@ module Suspect
     def register_listener(reporter)
       return unless supported_platform?
 
+      structure = build_structure
+      listener = build_listener(structure)
+
+      reporter.register_listener listener, *listener.notification_names
+    end
+
+    private
+
+    def build_structure
       root_path = ::Pathname.new('.')
       file_helper = ::Suspect::FileUtils::Idempotent.new
       structure = ::Suspect::Setup::Structure.new(root_path)
       collector_id_generator = ::Suspect::Setup::CollectorIdGenerator.new
       ::Suspect::Setup::Creator.new(structure, collector_id_generator, file_helper).build
 
+      structure
+    end
+
+    def build_listener(structure)
       storage_path = ::Suspect::Storage::DirPath.new(structure.storage_path, Time.now.utc)
+      file_helper = ::Suspect::FileUtils::Idempotent.new
       collector_id = file_helper.read(structure.collector_id_path)
       storage = ::Suspect::Storage::Appender.new(dir_path: storage_path, dir_helper: file_helper, collector_id: collector_id)
       file_tree = ::Suspect::FileTree::Git::Snapshot.new
-      listener = ::Suspect::Gathering::RSpec::Listener.new(file_tree, storage, collector_id, ::Time.now.utc)
 
-      reporter.register_listener listener, *listener.notification_names
+      ::Suspect::Gathering::RSpec::Listener.new(file_tree, storage, collector_id, ::Time.now.utc)
     end
-
-    private
 
     def supported_platform?
       ::Gem::Platform.local.os == 'linux'
